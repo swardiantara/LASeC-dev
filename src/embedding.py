c978561d708e36370e64876719d30bcef3ccc47f
swardiantara <swardyantara@gmail.com>
2025-11-22 21:22:48 +0700

    debug partial fine-tuning

--- src/embedding.py
+++ src/embedding.py
@@ -91,7 +91,7 @@ class LogEmbedding:
         selected_sample_path = os.path.join(self.output_path, f"{self.sampling_scenario}_selected_sample.xlsx")
         positive_pairs_path = os.path.join(self.output_path, f'{self.sampling_scenario}_positive_pairs.json')
         
-        print(f"Debug columns: {self.dataset.columns.tolist()}")
+        # print(f"Debug columns: {self.dataset.columns.tolist()}")
         # group by EventTemplate
         grouped = self.dataset.groupby(by='EventTemplate')
         
@@ -138,6 +138,7 @@ class LogEmbedding:
                 selected_sample = pd.concat([selected_sample, sample_messages], ignore_index=True)
 
             # save to file after finish sampling
+            print(f"Debug columns before saving: {selected_sample.columns.tolist()}")
             selected_sample = selected_sample.drop(['Content_embedding', 'EventTemplate_embedding'], axis='columns')
             selected_sample.to_excel(selected_sample_path)
             self._save_log_pairs(positive_pairs, positive_pairs_path)
